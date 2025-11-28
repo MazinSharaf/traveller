7bb2d693f17d1592a2cf757e47e2f6aa2575d2d8
Mazin Sharaf <mazinsharaf@icloud.com>
2025-11-28 13:03:37 +1100

    Add searchDestination function for travel recommendations

---
+++ travel_recommendation.js
@@ -0,0 +1,25 @@
+function searchDestination() {
+    const input = document.getElementById(
+        'searchInput'
+    ).value.toLowerCase();
+
+    const resultDiv = document.getElementById(
+        'result'
+    );
+
+    fetch (
+        'travel_recommendation_api.json'
+    )
+
+        .then (
+            response => response.json()
+        )
+
+        .then (
+            data => {
+                const destination = data.destinations.find(
+                    item => item.name.toLowerCase() === input
+                );
+            }
+        )
+}
