c754bddda01bf0ebcac4de6786fd751afd8babcb
Mazin Sharaf <mazinsharaf@icloud.com>
2025-12-06 13:22:22 +1100

    Enhance searchDestination to include cities, temples, and beaches
    Added functionality to search for cities, temples, and beaches in addition to countries.

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -21,10 +21,34 @@ function searchDestination() {
                     item => item.name.toLowerCase() === input
                 );
 
+                const city = data.countries.cities.find(
+                    item => item.name.toLowerCase === input
+                );
+
+                const temple = data.temples.find(
+                    item => item.name.toLowerCase() === input
+                );
+
+                const beach = data.beaches.find(
+                    item => item.name.toLowerCase() === input
+                );
+
                 if (
                     country
                 ) {
-                   console.log(country.name) 
+                    console.log(country.name) 
+                }
+
+                if (
+                    temple
+                ) {
+                    console.log(temple.name)
+                }
+
+                if (
+                    beach
+                ) {
+                    console.log(beach.name)
                 }
             }
         )
